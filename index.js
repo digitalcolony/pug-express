5536f941619b65f3443ea7577b4200033da505c2
MAS <digitalcolony@gmail.com>
2018-02-17 10:09:05 -0800

    Testing Extends and Includes

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ const pug = require("pug");
 
 const app = express();
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'pug');
-app.get('/', function (req, res) {
-    res.render('index', {
-        title: 'Pug Example',
-        message: 'ExpressJS with Pug',
-        name: 'Joe Sample',
-        tacos: ['chicken', 'pork', 'beef', 'fish']
-    })
+app.set("views", __dirname + "/views");
+app.set("view engine", "pug");
+app.get("/", function(req, res) {
+  res.render("index", {
+    title: "Pug Example",
+    message: "ExpressJS with Pug",
+    name: "Joe Sample",
+    tacos: ["chicken", "pork", "beef", "fish"]
+  });
 });
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+app.get("/about", function(req, res) {
+  res.render("about", {
+    title: "About Pug"
+  });
+});
+
+app.listen(3000, () => console.log("Listening on port 3000"));
